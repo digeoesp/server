bba9e728baeb22d3e2d63dc550365b8f8cdb4b5d
digeoesp <diegocrypto@outlook.com>
2021-05-05 23:15:47 -0700

    hid my keys

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
 
+//passport.use uses a google strategy
+//we have to give it a client id and client secret
+passport.use(new GoogleStrategy()); // new googlestartegy creates a new instance 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
