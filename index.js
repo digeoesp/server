c890efc2afd1cb5d25915244a513ab5b42fc1d37
digeoesp <diegocrypto@outlook.com>
2021-05-10 17:40:09 -0700

    setting up google auth

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+
+
+//MIDDLEWARE
 const app = express();
 
 
 //passport.use uses a google strategy
 //we have to give it a client id and client secret
-passport.use(new GoogleStrategy()); // new googlestartegy creates a new instance 
+// new googlestartegy creates a new instance 
+passport.use(
+    new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        //add propery callback the string is the route the user is sent to after they grant permisssion
+        callbackURL: '/auth/google/callback'
+    }, 
+    // as a second argument pass an arrow functiion
+    accessToken => {
+        console.log(accessToken);
+    }
+  )
+); 
+
+
+
+//route handler that make sure that user gets kicked into this passport flow
+//googleStrategy has an internal identifier as google
+app.get('/auth/google', passport.authenticate('google', {
+    //specifies to google what acces we want to have on this users profile 
+    scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
