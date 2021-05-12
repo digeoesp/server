f3b7a386021a40bc5654b908d43d6528bd2f5f32
digeoesp <diegocrypto@outlook.com>
2021-05-11 23:32:29 -0700

    rafactored code

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Sign In With Google</a>
+
         <a
           className="App-link"
           href="https://reactjs.org"
