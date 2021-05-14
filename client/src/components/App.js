dbc98b8a780e17acd52f52bde1314997e0771dee
digeoesp <diegocrypto@outlook.com>
2021-05-13 17:04:35 -0700

    auth button working on homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 
-const App = () => {
-    return (
-        <div>
-            <h1>hi there</h1>
-        </div>
-    );
+
+import Header from './Header';
+const Dashboard = () => <h2>dashboard</h2>
+const SurveyNew = () => <h2>surveynew</h2>
+const Landing = () => <h2>Landing</h2>
+
+
+
+
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,18 @@
+import materializeCSS from 'materialize-css/dist/css/materialize.min.css'
+
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './components/App'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-ReactDom.render(<App />, document.querySelector('#root'));
+import App from './components/App'
+import reducers from './reducers'
+
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDom.render(
+    <Provider store={store}><App /></Provider>,
+    document.querySelector('#root')
+);
