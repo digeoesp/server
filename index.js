f31d58debdb9c1c055977797b0c5817cc74ca1c8
digeoesp <diegocrypto@outlook.com>
2021-05-16 16:26:47 -0700

    heroku

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
-if (process.env.NODE_ENV === 'devoloper') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
