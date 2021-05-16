9596ce23c8102a09c41b21b31ebb7af4db05e092
digeoesp <diegocrypto@outlook.com>
2021-05-16 14:55:32 -0700

    added billing and client

--- models/User.js
+++ models/User.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 //we can add properties as we please
-const userSchema = new Schema ({
-    googleId: String
+const userSchema = new Schema({
+    googleId: String,
+    credits: { type: Number, default: 0 }
 });
 
 //to create an actual model class and tell mongoose that it needs to be aware that this new collection needs to be created
