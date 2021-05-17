bcc03838ee5c3c3cb1a59af69c44eb3e4aaf3170
digeoesp <diegocrypto@outlook.com>
2021-05-17 14:50:29 -0700

    fix server

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === 'production') {
+    appp.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 
